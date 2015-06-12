9790d4fbafe2b9382648292c84210aa530539de0
Jeroen Pullich <jeroen.pullich@hotmail.com>
2015-06-12 10:43:57 +0200

    Postbal, postdal

--- BAL/PostBAL.cs
+++ BAL/PostBAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,9 @@ namespace BAL
 {
     public class PostBAL
     {
+        public DataTable GetCategories()
+        {
+            return new PostDAL().LoadAll();
+        }
     }
 }
--- DAL/PostDAL.cs
+++ DAL/PostDAL.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
 
 namespace DAL
 {
     public class PostDAL
     {
+
+        #region Category Queries
+        public DataTable LoadRootCategories()
+        {
+            using (OracleConnection conn = new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString))
+            {
+                conn.Open();
+                string loadQuery = "SELECT * FROM Categorie WHERE category IS NULL";
+                using (OracleCommand cmd = new OracleCommand(loadQuery, conn))
+                {
+                    OracleDataAdapter a = new OracleDataAdapter(cmd);
+                    DataTable t = new DataTable();
+                    try
+                    {
+                        a.Fill(t);
+                        return t;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message.ToString());
+                        return t;
+                    }
+                }
+            }
+        }
+
+        public DataTable LoadChildCategories(string parentID)
+        {
+            using (OracleConnection conn = new OracleConnection(ConfigurationManager.ConnectionStrings["OracleConnectionString"].ConnectionString))
+            {
+                conn.Open();
+                string loadQuery = "SELECT * FROM Categorie WHERE category_id = :category_id";
+                using (OracleCommand cmd = new OracleCommand(loadQuery, conn))
+                {
+                    cmd.Parameters.Add(new OracleParameter("category_id", parentID));
+                    OracleDataAdapter a = new OracleDataAdapter(cmd);
+                    DataTable t = new DataTable();
+                    try
+                    {
+                        a.Fill(t);
+                        return t;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message.ToString());
+                        return t;
+                    }
+                }
+            }
+        }
+
+        #endregion
     }
 }
