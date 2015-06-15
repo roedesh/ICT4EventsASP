02a05cee6ed8ce3dd6abc2eafed61517fb7c8787
Poppel, Thom T.E. van <thom.vanpoppel@student.fontys.nl>
2015-06-15 21:52:56 +0200

    wat dingen in toegangscontrole
    Kleine verandering

--- ICT4Events/ToegangsControle.aspx.designer.cs
+++ ICT4Events/ToegangsControle.aspx.designer.cs
@@ -103,15 +103,6 @@ namespace ICT4Events {
         protected global::System.Web.UI.WebControls.Button btnShowAttendants;
         
         /// <summary>
-        /// imgPaymentStatus control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image imgPaymentStatus;
-        
-        /// <summary>
         /// lbPersonInfo control.
         /// </summary>
         /// <remarks>
