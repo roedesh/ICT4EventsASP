1a7d197326c99cee9eeae965985d7934569b0c7f
Verschueren,Berry B.C.H <b.verschueren@student.fontys.nl>
2015-06-16 08:53:19 +0200

    mail en ad ingevoegd in het project

---
+++ BAL/ActiveDirectoryBAL.cs
@@ -0,0 +1,231 @@
+﻿// <copyright file="ActiveDirectoryHandling.cs" company="ICT4EventsASP">
+//     Copyright (c) ADonderzoek. All rights reserved.
+// </copyright>
+// <author>Berry Verschueren</author>
+namespace BAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.DirectoryServices;
+    using System.DirectoryServices.ActiveDirectory;
+    class ActiveDirectoryBAL
+    {
+        /// <summary>
+        /// Class to handle active directory.
+        /// </summary>
+
+        /// <summary>
+        /// Integer value to work with.
+        /// </summary>
+        int counter;
+
+        /// <summary>
+        /// Array value to work with.
+        /// Usually containing all data returned and at the end a counter (1 or 0)
+        /// if counter equals 1 then an error occured.
+        /// </summary>
+        string[] returnValues;
+
+        /// <summary>
+        /// The path to use when working with active directory for our domain.
+        /// </summary>
+        const string url = "LDAP://CN=Users,DC=ICT4EVENTS,DC=PARENT";
+
+        /// <summary>
+        /// Initializes an instance of the ActiveDirectoryBAL class.
+        /// </summary>
+        public ActiveDirectoryBAL()
+        {
+            this.counter = 0;
+        }
+
+        /// <summary>
+        /// Method to create a user within the active directory, by default the account is disabled.
+        /// Do not forget to add the user to a certain group, using the AddToGroup method.
+        /// </summary>
+        /// <param name="username">username value</param>
+        /// <param name="password">password value</param>
+        /// <param name="email">email address value</param>
+        /// <returns>array with username, password, email and (1/0) errorOccurance index</returns>
+        public string[] CreateUser(string username, string password, string email)
+        {
+            try
+            {
+                using (DirectoryEntry localMachine = new DirectoryEntry(url, "Administrator", "FONTYSPTS-23", AuthenticationTypes.Secure))
+                using (DirectoryEntry newUser = localMachine.Children.Add("CN=" + username, "user"))
+                {
+                    newUser.Properties["samAccountName"].Value = username;
+                    newUser.CommitChanges();
+                    newUser.Invoke("SetPassword", new object[] { password });
+                    newUser.CommitChanges();
+                    newUser.Invoke("EmailAddress", new object[] { email });
+                    newUser.CommitChanges();
+                    newUser.Close();
+                    localMachine.Close();
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                this.counter++;
+            }
+            finally
+            {
+                this.returnValues = new string[] { username, password, email, this.counter.ToString() };
+            }
+
+            return this.returnValues;
+        }
+
+        /// <summary>
+        /// Method to delete a user from the active directory, only use if something went wrong, otherwise just disable the account.
+        /// </summary>
+        /// <param name="username">username value</param>
+        /// <returns>array with username and (1/0) errorOccurance index</returns>
+        public string[] DeleteUser(string username)
+        {
+            try
+            {
+                using (DirectoryEntry localMachine = new DirectoryEntry(url, "Administrator", "FONTYSPTS-23", AuthenticationTypes.Secure))
+                using (DirectoryEntry newUser = localMachine.Children.Add("CN=" + username, "user"))
+                {
+                    newUser.DeleteTree();
+                    newUser.Close();
+                    localMachine.Close();
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                this.counter++;
+            }
+            finally
+            {
+                this.returnValues = new string[] { username, this.counter.ToString() };
+            }
+
+            return this.returnValues;
+        }
+
+        /// <summary>
+        /// Method to see if the credentials are correct and recognized by the active directory server.
+        /// </summary>
+        /// <param name="username">username value</param>
+        /// <param name="password">password value</param>
+        /// <returns>array with username, password and (1/0) errorOccurance index</returns>
+        public string[] Authenticate(string username, string password)
+        {
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry(url, username, password);
+                object nativeObject = entry.NativeObject;
+                entry.Close();
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                this.counter++;
+            }
+            finally
+            {
+                this.returnValues = new string[] { username, password, this.counter.ToString() };
+            }
+
+            return this.returnValues;
+        }
+
+        /// <summary>
+        /// Method to enable a disabled account within the active directory. Enables for instance authentication.
+        /// </summary>
+        /// <param name="username">username value</param>
+        /// <returns>array with username and (1/0) errorOccurance index</returns>
+        public string[] EnableAccount(string username)
+        {
+            try
+            {
+                using (DirectoryEntry localMachine = new DirectoryEntry(url, "Administrator", "FONTYSPTS-23", AuthenticationTypes.Secure))
+                using (DirectoryEntry user = localMachine.Children.Find("CN=" + username, "user"))
+                {
+                    int val = (int)user.Properties["userAccountControl"].Value;
+                    user.Properties["userAccountControl"].Value = val & ~0x2;
+                    user.CommitChanges();
+                    user.Close();
+                    localMachine.Close();
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                this.counter++;
+            }
+            finally
+            {
+                this.returnValues = new string[] { username, this.counter.ToString() };
+            }
+
+            return this.returnValues;
+        }
+
+        /// <summary>
+        /// Method to disnable an enabled account within the active directory. Disables for instance authentication.
+        /// </summary>
+        /// <param name="username">username value</param>
+        /// <returns>array with username and (1/0) errorOccurance index</returns>
+        public string[] DisableAccount(string username)
+        {
+            try
+            {
+                using (DirectoryEntry localMachine = new DirectoryEntry(url, "Administrator", "FONTYSPTS-23", AuthenticationTypes.Secure))
+                using (DirectoryEntry user = localMachine.Children.Find("CN=" + username, "user"))
+                {
+                    int val = (int)user.Properties["userAccountControl"].Value;
+                    user.Properties["userAccountControl"].Value = val | 0x2;
+                    user.CommitChanges();
+                    user.Close();
+                    localMachine.Close();
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                this.counter++;
+            }
+            finally
+            {
+                this.returnValues = new string[] { username, this.counter.ToString() };
+            }
+
+            return this.returnValues;
+        }
+
+        /// <summary>
+        /// Method to add a user to a group within active directory.
+        /// </summary>
+        /// <param name="username">username value</param>
+        /// <param name="groupname">groupname value</param>
+        /// <returns>array with username, groupname and (1/0) errorOccurance index</returns>
+        public string[] AddToGroup(string username, string groupname)
+        {
+            try
+            {
+                using (DirectoryEntry localMachine = new DirectoryEntry(url, "Administrator", "FONTYSPTS-23", AuthenticationTypes.Secure))
+                using (DirectoryEntry group = localMachine.Children.Find("CN=" + groupname, "group"))
+                {
+                    string userDN = "LDAP://ICT4EVENTS.PARENT/CN=" + username + ",CN=Users,DC=ICT4EVENTS,DC=PARENT";
+                    group.Invoke("Add", new object[] { userDN });
+                    group.CommitChanges();
+                    group.Close();
+                    localMachine.Close();
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                this.counter++;
+            }
+            finally
+            {
+                this.returnValues = new string[] { username, groupname, this.counter.ToString() };
+            }
+
+            return this.returnValues;
+        }
+    }
+}
---
+++ BAL/MailBAL.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="MailBAL.cs" company="ICT4EventsASP">
+//     Copyright (c) mailwithhmailserver. All rights reserved.
+// </copyright>
+// <author>Berry Verschueren</author>
+namespace BAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Net.Mail;
+    using System.Net;
+    class MailBAL
+    {
+        /// <summary>
+        /// Array value to work with.
+        /// </summary>
+        string[] returnValues;
+
+        /// <summary>
+        /// Integer value to work with.
+        /// </summary>
+        int counter;
+
+        /// <summary>
+        /// Initializes an instance of the MailBAL class.
+        /// </summary>
+        public MailBAL()
+        {
+            this.counter = 0;
+        }
+
+        /// <summary>
+        /// Method to send an activation email to a specified emailadress.
+        /// </summary>
+        /// <param name="userID">userID value</param>
+        /// <param name="mailto">mailto value</param>
+        /// <param name="hash">hash value</param>
+        /// <returns>an array with the userID, mailto, hash and an (1/0) errorOccurance index</returns>
+        public string[] SendMail(string userID, string mailto, string hash)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("<br /><br />   Thank you for registering at <b>PTS23</b>. <br />To complete your registration, please follow the link below:<br />");
+            string link = string.Format("http://www.PTS23.com/Register.aspx?RegistrationCode={0}{1}",
+                                        userID.ToString(),
+                                        hash.ToString());
+            sb.AppendFormat(@"<a href=""{0}"">PTS23.com Complete Registration</a>", link);
+            sb.Append("<br /><br />When you have followed the link, you will be able to log in and use your account.<br />");
+            sb.Append("If your email system does not allow linking, please copy and paste the following into your browser:<br />");
+            sb.Append(link);
+            sb.Append("<br /><br />");
+
+            try
+            {
+                MailMessage msg = new MailMessage();
+                msg.Subject = "Activation E-mail";
+                msg.From = new MailAddress("fontyspts23@gmail.com");
+                msg.Body = sb.ToString();
+                msg.To.Add(new MailAddress(mailto));
+                msg.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                NetworkCredential loginCredentials = new NetworkCredential("fontyspts23@gmail.com", "PTS23PTS23");
+                smtp.Credentials = loginCredentials;
+                smtp.Send(msg);
+            }
+            catch (SmtpException)
+            {
+                this.counter++;
+            }
+            finally
+            {
+                this.returnValues = new string[] { userID, mailto, hash, this.counter.ToString() };
+            }
+
+            return this.returnValues;
+        }
+    }
+}
