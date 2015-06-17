e26f0acfd97d1a90983ec06cf0df02fc1619ddaf
Verschueren,Berry B.C.H <b.verschueren@student.fontys.nl>
2015-06-17 21:43:18 +0200

    usings toegevoegd in locationdal in guiclass

--- ICT4Events/Event/CreateNewLocation.aspx.cs
+++ ICT4Events/Event/CreateNewLocation.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BAL;
 
 namespace ICT4Events
 {
