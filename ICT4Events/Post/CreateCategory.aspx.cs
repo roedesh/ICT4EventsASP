5820e10a50af7016ea527d6bd94c32ad0f7fb48e
Koch, Sander A.D <s.koch@student.fontys.nl>
2015-06-18 14:31:20 +0200

    CreateCategoryfixed

--- ICT4Events/Post/CreateCategory.aspx.cs
+++ ICT4Events/Post/CreateCategory.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using BAL;
 
 namespace ICT4Events.Post
 {
@@ -11,7 +13,24 @@ namespace ICT4Events.Post
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataTable categories = new PostBAL().GetAllCategories();
+            ddlCategory.DataSource = categories;
+            ddlCategory.DataTextField = "NAAM";
+            ddlCategory.DataValueField = "NAAM";
+            ddlCategory.DataBind();
+            ddlCategory.Items.Insert(0, "");
+        }
 
+        protected void btnCategory_Click(object sender, EventArgs e)
+        {
+            if(new PostBAL().CreateCategory((Session["User_ID"].ToString()), ddlCategory.SelectedValue, tbCategory.Text) == 0)
+            {
+                Response.Write("<script language=javascript>alert('Er ging wat fout met het toevoegen van de categorie');</script>");
+            }
+            else
+            {
+                Response.Write("<script language=javascript>alert('Categorie is toegevoegd');</script>");
+            }
         }
     }
 }
--- ICT4Events/Post/CreateCategory.aspx.designer.cs
+++ ICT4Events/Post/CreateCategory.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace ICT4Events.Post {
     public partial class CreateCategory {
         
         /// <summary>
+        /// ddlCategory control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategory;
+        
+        /// <summary>
         /// tbCategory control.
         /// </summary>
         /// <remarks>
